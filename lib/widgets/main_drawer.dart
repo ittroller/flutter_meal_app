14c31a7c8cb1d2c60f65aa31d74030979d57d1f5
Cao Kha Minh <ittroller8@gmail.com>
2021-02-27 21:32:31 +0700

    first commit

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../screens/filters_screen.dart';
+
+class MainDrawer extends StatefulWidget {
+  @override
+  _MainDrawerState createState() => _MainDrawerState();
+}
+
+class _MainDrawerState extends State<MainDrawer> {
+  Widget buildListTile(String title, IconData icon, Function tapHandle) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: tapHandle,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // safearea to show header app
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'aasvdv',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile('Meals', Icons.restaurant, () {
+            Navigator.of(context).pushNamed('/');
+          }),
+          buildListTile('Filters', Icons.settings, () {
+            // pushReplacementNamed : not show icon back
+            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+          }),
+        ],
+      ),
+    );
+  }
+}
